34d1df9a1b94095797afa820df1882245147b53b
EFI-Tom <tavsic@yahoo.de>
2026-05-09 16:03:26 +0200

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+document.getElementById("main-nav").innerHTML = `
+<nav>
+    <a href="index.html">Home</a>
+    <a href="gallery.html">Galerie</a>
+    <a href="downloads.html">Downloads</a>
+    <a href="docs.html">Dokumentation</a>
+    <a href="howto_blitzerdepro.html">Blitzer.de App</a>
+    <a href="about.html">Die Story</a>
+    <a href="impressum.html">Impressum</a>
+</nav>
+`;
